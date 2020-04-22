56525c3802deb0cd35ab8f99c0ec0160627e3a43
Lucas Gothelipe <57392406+lgothelipe@users.noreply.github.com>
2020-04-23 01:27:44 +1000

    Update push_image.sh

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -1,9 +1,9 @@
-repo_name=cat ./terraform/aws-ecr/repo.txt | sed -n -e 's/^.*repository_url = //p'
-repo_pass=cat ./terraform/aws-ecr/repo.txt | sed -n -e 's/^.*repo_pass = //p'
+repo_name=$(cat ./terraform/aws-ecr/repo.txt | sed -n -e 's/^.*repository_url = //p')
+repo_pass=$(cat ./terraform/aws-ecr/repo.txt | sed -n -e 's/^.*repo_pass = //p')
 
 docker login --username AWS --password $repo_pass $repo_name
 docker build -t wordpress:latest -f ./docker/wordpress/Dockerfile .
 docker tag wordpress:latest $repo_name:latest
 docker push $repo_name:latest
 
-rm -f ./terraform/aws-ecr/repo.tx
+rm -f ./terraform/aws-ecr/repo.txt
