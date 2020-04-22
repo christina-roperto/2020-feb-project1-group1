88d9558bd72873c8e3f18c701fefe65f96b45843
lgothelipe <57392406+lgothelipe@users.noreply.github.com>
2020-04-23 01:23:46 +1000

    get ecr password running on docker

--- scripts/apply_ecr.sh
+++ scripts/apply_ecr.sh
@@ -1,3 +1,4 @@
 cd /code/terraform/aws-ecr
 terraform apply ecr
 terraform output > repo.txt
+echo repo_pass = $(aws ecr get-login-password --region $AWS_REGION) >> repo.txt
--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -1,6 +1,7 @@
-repo_name=$(cut -d " " -f 3 ./terraform/aws-ecr/repo.txt)
+repo_name=cat ./terraform/aws-ecr/repo.txt | sed -n -e 's/^.*repository_url = //p'
+repo_pass=cat ./terraform/aws-ecr/repo.txt | sed -n -e 's/^.*pass = //p'
 
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin $repo_name
+docker login --username AWS --password $repo_pass $repo_name
 docker build -t wordpress:latest -f ./docker/wordpress/Dockerfile .
 docker tag wordpress:latest $repo_name:latest
 docker push $repo_name:latest
