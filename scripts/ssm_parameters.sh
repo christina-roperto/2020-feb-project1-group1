59697538213f6e0efbe6028c91ac54fe6ba88866
root <root@vm.mshome.net>
2020-04-26 14:11:14 +1000

    fix ssm

--- scripts/ssm_parameters.sh
+++ scripts/ssm_parameters.sh
@@ -1,2 +1,10 @@
-aws ssm put-parameter --name "PROJ1_DB_USER" --value "admin$RANDOM" --type SecureString --overwrite
-aws ssm put-parameter --name "PROJ1_DB_PASSWORD" --value "wdpress$RANDOM$RANDOM" --type SecureString --overwrite
+#!/bin/bash
+if ! aws ssm get-parameter --name "PROJ1_DB_USER"
+then
+    aws ssm put-parameter --name "PROJ1_DB_USER" --value "admin$RANDOM" --type SecureString
+fi
+
+if ! aws ssm get-parameter --name "PROJ1_DB_PASSWORD"
+then
+    aws ssm put-parameter --name "PROJ1_DB_PASSWORD" --value "wdpress$RANDOM$RANDOM" --type SecureString
+fi
