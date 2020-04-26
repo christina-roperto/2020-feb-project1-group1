76130a20294536f976ef414a4ce4c58b817f7ac5
Lucas Gothelipe <57392406+lgothelipe@users.noreply.github.com>
2020-04-26 10:34:16 +1000

    Update ssm_parameters.sh

--- scripts/ssm_parameters.sh
+++ scripts/ssm_parameters.sh
@@ -1,2 +1,2 @@
 aws ssm put-parameter --name "PROJ1_DB_USER" --value "admin$RANDOM" --type SecureString --overwrite
-aws ssm put-parameter --name "PROJ1_DB_PASSWORD" --value "$RANDOM$RANDOM" --type SecureString --overwrite
+aws ssm put-parameter --name "PROJ1_DB_PASSWORD" --value "wdpress$RANDOM$RANDOM" --type SecureString --overwrite
