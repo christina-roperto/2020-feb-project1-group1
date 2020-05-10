96b461577f461f3a588427b5e1072cf71295efee
Christina Thee Roperto <christina.thee@gmail.com>
2020-05-10 15:29:17 +1000

    making docker build run faster

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -2,6 +2,6 @@ repo_name=$(aws ssm get-parameter --name "PROJ1_ECR_URL" --with-decryption --out
 repo_pass=$(aws ecr get-login-password --region ap-southeast-2)
 
 docker login --username AWS --password $repo_pass $repo_name
-docker build -t wordpress:latest -f ./docker/wordpress/Dockerfile .
+docker build -t wordpress:latest docker/wordpress
 docker tag wordpress:latest $repo_name:latest
 docker push $repo_name:latest
